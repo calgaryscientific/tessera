40833680407bc88d9af9ea2fda231b8bd013e9e2
Chris Jarabek <chris.jarabek@gmail.com>
2014-09-08 16:08:29 -0600

    Unbind() from appstate.

--- tessera.js
+++ tessera.js
@@ -2,25 +2,41 @@
 
 angular.module('tessera', [])
     .service('$tessera', function() {
-    	var unwatch = null;
+    	var paths = {};
 		this.bind = function(scope, prop, path, handler){			
+			var bound = {
+				handler: handler,
+				property: prop,
+				unlisten: null
+			};
+
 			pureweb.getFramework().getState().getStateManager().addValueChangedHandler(path, function(evt){
 				var newVal = evt.getNewValue();
 				if (scope[prop] !== newVal){
 					scope[prop] = newVal;
 					scope.$apply();
 				}
-				if ((handler !== null) && (typeof handler !== undefined)){
+				if ((handler !== null) && (typeof handler !== 'undefined')){
 					handler(evt);
 				}
-			});
+			});			
 			
-			unwatch = scope.$watch(prop, function(newVal, oldVal){
+			bound.unlisten = scope.$watch(prop, function(newVal, oldVal){
 				if (newVal === oldVal){
 					return;
 				}
 				pureweb.getFramework().getState().setValue(path, newVal);
 			});
-		};		
+			paths[path] = bound;
+		};
+
+		this.unbind = function(prop, path){
+			var bound = paths[path];
+			if ((bound !== null) && (typeof bound !== 'undefined')){
+				if (typeof bound.unlisten === 'function'){
+					bound.unlisten();
+				}
+			}
+		};
 	}	   
 );
