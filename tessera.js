4863d845c72baa4584547e0ad1e1fa9688433f3d
Chris Jarabek <chris.jarabek@gmail.com>
2014-09-08 17:24:08 -0600

    Unbind() now correctly detaches appstate handler as well

--- tessera.js
+++ tessera.js
@@ -7,19 +7,22 @@ angular.module('tessera', [])
 			var bound = {
 				handler: handler,
 				property: prop,
+				boundHandler: null,
 				unlisten: null
 			};
 
-			pureweb.getFramework().getState().getStateManager().addValueChangedHandler(path, function(evt){
+			var appStateHandler = function(evt){
 				var newVal = evt.getNewValue();
 				if (scope[prop] !== newVal){
 					scope[prop] = newVal;
 					scope.$apply();
 				}
 				if ((handler !== null) && (typeof handler !== 'undefined')){
-					handler(evt);
+					handler(evt);					
 				}
-			});			
+			}
+			bound.boundHandler = appStateHandler;
+			pureweb.getFramework().getState().getStateManager().addValueChangedHandler(path, appStateHandler);
 			
 			bound.unlisten = scope.$watch(prop, function(newVal, oldVal){
 				if (newVal === oldVal){
@@ -27,15 +30,27 @@ angular.module('tessera', [])
 				}
 				pureweb.getFramework().getState().setValue(path, newVal);
 			});
+			
 			paths[path] = bound;
+			return bound;
 		};
 
-		this.unbind = function(prop, path){
+		this.unbind = function(prop, path, handler){
 			var bound = paths[path];
 			if ((bound !== null) && (typeof bound !== 'undefined')){
 				if (typeof bound.unlisten === 'function'){
 					bound.unlisten();
 				}
+			}
+
+			//If you passed in an AppState handler, and it's one that's registered
+			if ((handler !== null) && (typeof handler !== 'undefined') && (handler === bound.handler)){
+				//Unregister it
+				pureweb.getFramework().getState().getStateManager().removeValueChangedHandler(path, bound.boundHandler);				
+			} 
+			//Else, if there is a handler registered for the thing you just unbound, pass it back.
+			else if ((bound.handler !== null) && (typeof bound.handler !== 'undefined')){
+				return bound.handler;
 			}
 		};
 	}	   
